c53bd922be4a487efbaa94fd4de9bb3f76906bbd
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-11-16 15:59:12 -0500

    Add Services

---
+++ MvcInvoice/Services/CompanyService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcInvoice.Models;
+using MvcInvoice.Repositories;
+
+namespace MvcInvoice.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private ICompanyRepository repo = null;
+
+        public void Create(Company entity)
+        {
+            using(repo = new CompanyRepository())
+            {
+                repo.Create(entity);
+            }
+        }
+
+        public void Delete(Company entity)
+        {
+            using(repo = new CompanyRepository())
+            {
+                repo.Delete(entity);
+            }
+        }
+
+        public Company Find(int id)
+        {
+            using(repo = new CompanyRepository())
+            {
+                return repo.Find(id);
+            }
+        }
+
+        public IEnumerable<Company> Read()
+        {
+            using(repo = new CompanyRepository())
+            {
+                return repo.Read();
+            }
+        }
+
+        public void Update(Company entity)
+        {
+            using(repo = new CompanyRepository())
+            {
+                repo.Update(entity);
+            }
+        }
+    }
+}
---
+++ MvcInvoice/Services/IService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcInvoice.Services
+{
+    public interface IService<T,ID>
+    {
+        void Create(T entity);
+
+        T Find(ID id);
+
+        IEnumerable<T> Read();
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
